4a446d389b8a5e175d2a9e394b619887c6a14a11
lskolj <lovreskoljarev9@gmail.com>
2026-08-15 23:02:00 +0200

    Dodan axios

---
+++ frontend/src/boot/axios.js
@@ -0,0 +1,19 @@
+import { boot } from 'quasar/wrappers'
+import axios from 'axios'
+
+const api = axios.create({ baseURL: 'http://localhost:9000/api' })
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default boot(({ app }) => {
+  app.config.globalProperties.$axios = axios
+  app.config.globalProperties.$api = api
+})
+
+export { api }
